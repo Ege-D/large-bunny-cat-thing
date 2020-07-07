92d3c6462fe97da6aaf62234428b1db4aedfa1cf
ege <egetat@gmail.com>
2020-07-07 15:03:11 +0300

    firebase.auth error

--- app/src/main/java/com/ege/firebasetest/versioncheck/HomeActivity.kt
+++ app/src/main/java/com/ege/firebasetest/versioncheck/HomeActivity.kt
@@ -12,12 +12,18 @@ import com.google.android.gms.auth.api.signin.GoogleSignInClient
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions
 import com.google.android.gms.common.api.ApiException
 import com.google.android.gms.tasks.Task
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.GoogleAuthProvider
+import com.google.firebase.ktx.Firebase
 import kotlinx.android.synthetic.main.activity_home.*
 
 
 // ID: 936002256726-6a303763b968mcrera403vvppdhekq5l.apps.googleusercontent.com
 // Secret: 60cchU6aBbjgZ9ip0fFdi0BA
 class HomeActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
@@ -25,11 +31,22 @@ class HomeActivity : AppCompatActivity() {
         homeRegisterBtn.isClickable = registerBoolean
         val gso =
             GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestIdToken(getString(R.string.default_web_client_id))
                 .requestEmail()
                 .build()
         val googleSignInClient = GoogleSignIn.getClient(this, gso);
         homeLoginBtn.setOnClickListener(GoogleSignInListener(googleSignInClient))
+        auth = Firebase.auth
 
+
+    }
+    override fun onStart() {
+        super.onStart()
+        // Check if user is signed in (non-null) and update UI accordingly.
+        val currentUser = auth.currentUser
+        if (currentUser != null) {
+            textView.text = currentUser.displayName.toString()
+        }
     }
 
     inner class GoogleSignInListener(private val googleSignInClient : GoogleSignInClient) : View.OnClickListener {
@@ -55,11 +72,44 @@ class HomeActivity : AppCompatActivity() {
             // a listener.
             val task =
                 GoogleSignIn.getSignedInAccountFromIntent(data)
-            handleSignInResult(task)
+            try {
+                // Google Sign In was successful, authenticate with Firebase
+                val account = task.getResult(ApiException::class.java)!!
+                Log.d("SCS:", "firebaseAuthWithGoogle:" + account.id)
+                firebaseAuthWithGoogle(account.idToken!!)
+            } catch (e: ApiException) {
+                // Google Sign In failed, update UI appropriately
+                Log.w("ERR:", "Google sign in failed", e)
+                // ...
+            }
+
         }
     }
 
-    private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
+    private fun firebaseAuthWithGoogle(idToken: String) {
+        val credential = GoogleAuthProvider.getCredential(idToken, null)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(this) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d("SCS", "signInWithCredential:success")
+                    val user = auth.currentUser
+                    if (user != null) {
+                        textView.text = user.displayName.toString()
+                    }
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w("ERR:", "signInWithCredential:failure", task.exception)
+                    // ...
+                    Snackbar.make(homeLayout, "Authentication Failed.", Snackbar.LENGTH_SHORT).show()
+                    textView.text = "Hello World"
+                }
+
+                // ...
+            }
+    }
+
+   /* private fun handleSignInResult(completedTask: Task<GoogleSignInAccount>) {
         try {
             val account =
                 completedTask.getResult(ApiException::class.java)
@@ -80,5 +130,5 @@ class HomeActivity : AppCompatActivity() {
             //updateUI(null)
             textView.text = "Hello World"
         }
-    }
+    }*/
 }
