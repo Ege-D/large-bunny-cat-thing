c262c425488a24349614082869d04eefaa5e2b3a
ege <egetat@gmail.com>
2020-07-07 11:03:14 +0300

    button text

--- app/src/main/java/com/ege/firebasetest/MainActivity.kt
+++ app/src/main/java/com/ege/firebasetest/MainActivity.kt
@@ -2,10 +2,30 @@ package com.ege.firebasetest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import com.google.firebase.ktx.Firebase
+import com.google.firebase.remoteconfig.FirebaseRemoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfig
+import com.google.firebase.remoteconfig.ktx.remoteConfigSettings
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var remoteConfig: FirebaseRemoteConfig
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        remoteConfig = Firebase.remoteConfig
+        val configSettings = remoteConfigSettings {
+            minimumFetchIntervalInSeconds = 3600
+        }
+        remoteConfig.setConfigSettingsAsync(configSettings)
+        remoteConfig.setDefaultsAsync(R.xml.remote_config_defaults)
     }
+
+    fun buttonClicked(view: View) {
+        button.text = remoteConfig.getString("test_success")
+    }
+
+
 }
